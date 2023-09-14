90832aad4011f51414756aaa4fcb3a6ecb4e8a90
Ashutosh Dubey <ashutoshdubey133@gmail.com>
2023-09-14 20:32:45 +0530

    added impl to pmdaoimpl

--- Backend/src/in/co/hsbc/bts/dao/ProjectManagerDao.java
+++ Backend/src/in/co/hsbc/bts/dao/ProjectManagerDao.java
@@ -7,6 +7,7 @@ import in.co.hsbc.bts.model.ProjectManager;
 public interface ProjectManagerDao {
 	boolean create(ProjectManager projectManager);
 	ProjectManager getById(int id);
+	ProjectManager getByEmail(String email);
 	boolean update(ProjectManager projectManager);
 	boolean deleteById(int id);
 	Set<ProjectManager> findAll();
--- Backend/src/in/co/hsbc/bts/dao/impl/ProjectManagerDaoImpl.java
+++ Backend/src/in/co/hsbc/bts/dao/impl/ProjectManagerDaoImpl.java
@@ -1,7 +1,10 @@
 package in.co.hsbc.bts.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashSet;
 import java.util.Set;
 
 import in.co.hsbc.bts.dao.ProjectManagerDao;
@@ -28,8 +31,28 @@ public class ProjectManagerDaoImpl implements ProjectManagerDao {
 
 	@Override
 	public ProjectManager getById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			String query = "SELECT * FROM pm_tbl INNER JOIN user_tbl WHERE pmId = id AND id = ?";
+			
+			PreparedStatement stmt = con.prepareStatement(query);
+			stmt.setInt(1, id);
+			
+			ResultSet result = stmt.executeQuery();
+			
+			ProjectManager pm = new ProjectManager();
+			
+			pm.setUserId(result.getInt(2));
+			pm.setUserName(result.getString(3));
+			pm.setUserEmail(result.getString(4));
+			pm.setPassword(result.getString(5));
+			
+			return pm;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+//			TODO: throw exception
+			return null;
+		}
 	}
 
 	@Override
@@ -46,8 +69,59 @@ public class ProjectManagerDaoImpl implements ProjectManagerDao {
 
 	@Override
 	public Set<ProjectManager> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			String query = "SELECT * FROM pm_tbl INNER JOIN user_tbl WHERE pmId = id";
+			
+			PreparedStatement stmt = con.prepareStatement(query);
+			
+			ResultSet result = stmt.executeQuery();
+			
+			Set<ProjectManager> set = new HashSet<>();
+			
+			while(result.next()) {
+				ProjectManager pm = new ProjectManager();
+				
+				pm.setUserId(result.getInt(2));
+				pm.setUserName(result.getString(3));
+				pm.setUserEmail(result.getString(4));
+				pm.setPassword(result.getString(5));
+				
+				set.add(pm);
+			}
+			
+			return set;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+//			TODO: throw exception
+			return null;
+		}
+	}
+
+	@Override
+	public ProjectManager getByEmail(String email) {
+		try {
+			String query = "SELECT * FROM pm_tbl INNER JOIN user_tbl WHERE pmId = id AND name = ?";
+			
+			PreparedStatement stmt = con.prepareStatement(query);
+			stmt.setString(1, email);
+			
+			ResultSet result = stmt.executeQuery();
+			
+			ProjectManager pm = new ProjectManager();
+			
+			pm.setUserId(result.getInt(2));
+			pm.setUserName(result.getString(3));
+			pm.setUserEmail(result.getString(4));
+			pm.setPassword(result.getString(5));
+			
+			return pm;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+//			TODO: throw exception
+			return null;
+		}
 	}
 
 }
