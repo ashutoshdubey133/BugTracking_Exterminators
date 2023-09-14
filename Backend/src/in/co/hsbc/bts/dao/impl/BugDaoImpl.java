00b589370684b687f3caa151e1847613789432e3
vaibhavimekhe <vaibhavimekhe@gmail.com>
2023-09-14 23:12:31 +0530

    updated BugDaoImpl

--- Backend/src/in/co/hsbc/bts/dao/impl/BugDaoImpl.java
+++ Backend/src/in/co/hsbc/bts/dao/impl/BugDaoImpl.java
@@ -1,34 +1,132 @@
 package in.co.hsbc.bts.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
 
 import in.co.hsbc.bts.dao.BugDao;
 import in.co.hsbc.bts.factory.Factory;
 import in.co.hsbc.bts.model.Bug;
+import in.co.hsbc.bts.model.Developer;
+import in.co.hsbc.bts.model.Project;
+import in.co.hsbc.bts.model.ProjectManager;
+import in.co.hsbc.bts.model.Tester;
+import in.co.hsbc.bts.model.BugStatus;
 
 public class BugDaoImpl implements BugDao {
 	
 	private Connection con = null;
+	private ProjectDaoImpl projectDaoImpl;
+	private TesterDaoImpl testerDaoImpl;
+	private DeveloperDaoImpl developerDaoImpl;
+	private ProjectManagerDaoImpl projectManagerDaoImpl;
 	
 	public BugDaoImpl() {
-		try {
+		try 
+		{
 			con = Factory.getJdbcConnection();
-		} catch (ClassNotFoundException | SQLException e) {
+			projectDaoImpl = new ProjectDaoImpl();
+			testerDaoImpl = new TesterDaoImpl();
+			developerDaoImpl = new DeveloperDaoImpl();
+			projectDaoImpl = new ProjectDaoImpl();
+		} 
+		catch (ClassNotFoundException | SQLException e) 
+		{
 			e.printStackTrace();
 		}
 	}
 	
 	@Override
 	public boolean create(Bug bug) {
-		// TODO Auto-generated method stub
+		String query  = "INSERT INTO bug_tbl VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+		try 
+		{
+			PreparedStatement stmt = con.prepareStatement(query);
+			
+			stmt.setInt(1, bug.getId());
+			stmt.setString(2, bug.getTitle());
+			stmt.setString(3, bug.getDescription());
+			stmt.setInt(4, bug.getProject().getProjectId());
+			stmt.setInt(5, bug.getCreatedBy().getUserId());
+			stmt.setDate(6,convertUtilToSql( bug.getOpenDate()));
+			stmt.setInt(7,  bug.getAssignedTo().getUserId());
+			stmt.setInt(8, bug.getMarkedForClosingBy().getUserId());
+			stmt.setInt(9, bug.getClosedBy().getUserId());
+			stmt.setDate(10, convertUtilToSql(bug.getClosedOn()));
+			//stmt.set	Enum
+			//stmt.set	Enum
+			int inserted = stmt.executeUpdate();
+			if(inserted==1) {
+				return true;
+			}
+		} 
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			//TODO exception
+		}
+		
 		return false;
 	}
 
+	public Date convertSqlToUtilDate(java.sql.Date sqlDate) {
+		java.util.Date utilDate = new java.util.Date(sqlDate.getTime());
+		return utilDate;
+	}
+	
+	public java.sql.Date convertUtilToSql(java.util.Date utilDate){
+		java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+		return sqlDate;
+	}
 	@Override
 	public Bug getById(int id) {
-		// TODO Auto-generated method stub
+		String query = "SELECT * FROM bug_tbl WHERE bugId = ?";
+		
+		try {
+			PreparedStatement stmt = con.prepareStatement(query);
+			stmt.setInt(1, id);
+			ResultSet result = stmt.executeQuery();
+			
+			Bug bug = new Bug();
+			bug.setTitle(result.getString("title"));
+			bug.setDescription(result.getString("description"));
+			
+			Project p = projectDaoImpl.getById(result.getInt("projectId"));
+			bug.setProject(p);
+			
+			Tester tester = testerDaoImpl.getById(result.getInt("createdBy"));
+			bug.setCreatedBy(tester);
+			
+			bug.setOpenDate(convertSqlToUtilDate(result.getDate("openDate")));
+			
+			Developer developer = developerDaoImpl.getById(result.getInt("assignedTo"));
+			bug.setAssignedTo(developer);
+			
+			Developer developerMarkedForClosing = developerDaoImpl.getById(result.getInt("markedForClosingBy"));
+			bug.setMarkedForClosingBy(developerMarkedForClosing);
+			
+			ProjectManager projectManager = projectManagerDaoImpl.getById(result.getInt("closedBy"));
+			bug.setClosedBy(projectManager);
+			
+			bug.setClosedOn(convertSqlToUtilDate(result.getDate("closedOn")));
+			
+			//bug.setBugStatus(null);
+			
+			//bug.setSeverityLevel(null);
+			
+			return bug;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			//TODO: throw Exception
+		}
+		
 		return null;
 	}
 
@@ -46,7 +144,53 @@ public class BugDaoImpl implements BugDao {
 
 	@Override
 	public Set<Bug> findAll() {
-		// TODO Auto-generated method stub
+		
+		String query = "SELECT * FROM bug_tbl";
+		try 
+		{
+			PreparedStatement stmt = con.prepareStatement(query);
+			ResultSet result = stmt.executeQuery();
+			Set<Bug> bugSet = new HashSet<>();
+			while(result.next()) {
+				Bug bug = new Bug();
+				
+				bug.setTitle(result.getString("title"));
+				bug.setDescription(result.getString("description"));
+				
+				Project p = projectDaoImpl.getById(result.getInt("projectId"));
+				bug.setProject(p);
+				
+				Tester tester = testerDaoImpl.getById(result.getInt("createdBy"));
+				bug.setCreatedBy(tester);
+				
+				bug.setOpenDate(convertSqlToUtilDate(result.getDate("openDate")));
+				
+				Developer developer = developerDaoImpl.getById(result.getInt("assignedTo"));
+				bug.setAssignedTo(developer);
+				
+				Developer developerMarkedForClosing = developerDaoImpl.getById(result.getInt("markedForClosingBy"));
+				bug.setMarkedForClosingBy(developerMarkedForClosing);
+				
+				ProjectManager projectManager = projectManagerDaoImpl.getById(result.getInt("closedBy"));
+				bug.setClosedBy(projectManager);
+				
+				bug.setClosedOn(convertSqlToUtilDate(result.getDate("closedOn")));
+				
+				//bug.setBugStatus(null);
+				
+				//bug.setSeverityLevel(null);
+				
+				bugSet.add(bug);
+			}
+			return bugSet;
+		} 
+		catch (SQLException e) {
+			
+			e.printStackTrace();
+			//TODO throw exception
+			
+		}
+		
 		return null;
 	}
 
--- Backend/src/module-info.java
+++ Backend/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module BugTracking_Exterminators {
+	requires java.sql;
 }
